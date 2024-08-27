4448d8f6ba404e1a92601a82a334ce5ad783c265
BERK <guneybrk95@gmail.com>
2024-08-28 00:55:32 +0300

    First

---
+++ app/Program.cs
@@ -0,0 +1,21 @@
+﻿//Inspect contents of assembly (types, methods, properties)
+//Not directly referenced
+//Bypass accses modifiers
+using System.Reflection; 
+public class Program{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(typeof(Program).Assembly.GetTypes()); 
+    }
+}
+
+public class Exercise{
+    public class Player{
+        private void NewFunc()
+        {
+            Console.WriteLine("NewFunc");
+        }
+    }
+    private class Player2{}
+    private class Player3{}
+}
